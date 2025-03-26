a30086394c960dd7aff3ad6afc483d64a9d53c83
kylie-c-warden <kwarden@drury.edu>
2025-03-26 14:56:10 -0500

    combined graphs

--- Main Model.py
+++ Main Model.py
@@ -3,36 +3,16 @@ import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.integrate import odeint
 import numpy as np
+from reproductive_number import *
+
+
 
 def odes (x, t): 
-    # constants 
-    u_c = 0.0743
-    u_p = 0.015
-    u_i = 0.025
-
-    S_a = 0.0834
-    S_c = 0.0233   
-    S_p = 0.0039  #0.00399995 
-    S_i = 0.0527
-
-    alpha_c = 0.031   #keep curcumin at a range of 0.1 to 0.2
-
-    Beta = 0.12725  #0.0001597
-
-    r = 0.0829
-
-    lambda_p = 0.00915
-    lambda_i = 0.0413536
-
-   
-
 
     A = x[0]
     P = x[1]
     C = x[2]
     I = x[3]
-
-
 
     # our ODEs 
     dPdt = lambda_p - S_p * P - Beta * C * P   #good 
@@ -42,6 +22,25 @@ def odes (x, t):
 
     return [dAdt, dPdt, dCdt, dIdt]
 
+#Sensitivity of parameters:
+S_alphaC = (alpha_c / r_0)* ((S_i*(-1)*(2*S_p*u_c - Beta*lambda_p)) / (S_p*S_c*lambda_i*(1+alpha_c)**2))
+
+S_beta = (Beta / r_0)*((lambda_p*S_i)/(S_p*S_c*(1+alpha_c)*lambda_i))
+
+S_deltaC = (S_c / r_0)*(-1)*(S_i*(u_c*(1-alpha_c)*S_p+Beta*lambda_p)/(((S_c)**2)*(1+alpha_c)*lambda_i*S_p))
+
+S_deltaI = (S_i / r_0)*((u_c*(1-alpha_c)*S_p+Beta*lambda_p)/(S_c*S_p*(1+alpha_c)*lambda_i))
+
+S_deltaP = (S_p / r_0)*(-1)*((Beta*lambda_p*S_i) / ((S_c*(S_p)**2) *(1+alpha_c)*lambda_i))
+
+S_lambdaI = (lambda_i / r_0)*(-1)*S_i*((u_c*(1-alpha_c)+Beta*(lambda_p / S_p))/(S_c*(1+alpha_c)*((lambda_i)**2)))
+
+S_lambdaP = (lambda_p / r_0)*(Beta*S_i/S_p*S_c*(1+alpha_c)*lambda_i)
+
+S_muC = (u_c/r_0)*((S_i*(1-alpha_c))/(S_c*(1+alpha_c)*lambda_i))
+
+params = dict(aC = S_alphaC, B = S_beta, dC = S_deltaC, dI = S_deltaI, dP = S_deltaP, li = S_lambdaI, lP = S_lambdaP, mC = S_muC)
+print(params)
 #  initial conditions 
 # x_0 = [a_0, p_0, c_0, i_0] 
 
@@ -53,16 +52,14 @@ tf = 100
 t = np.linspace(0,tf,1000)
 x = odeint(odes, x_0, t)
 
-
 A = x[:,0]
 P = x[:,1]
 C = x[:,2]
 I = x[:,3]
 
 
-
 # # Plotting each equation in a separate subplot with a log y-axis
-fig, axis = plt.subplots(2, 2, figsize=(12, 10))
+fig, axis = plt.subplots(2, 3, figsize=(14, 7))
 
 # Plot P(t)
 axis[0, 0].plot(t, P, color='blue')
@@ -89,12 +86,19 @@ axis[1, 0].set_ylabel('$C(t)$')
 axis[1, 0].grid()
 
 # Turn off the last empty subplot
-
 axis[1,1].plot(t, I, color='purple')
 axis[1,1].set_title('$I(t)$ Graph')
 axis[1, 1].set_xlabel('$t$')
 axis[1,1].set_ylabel('$I(t)$')
 axis[1,1].grid()
+
+#sensitivity
+x = params.keys()
+y = params.values()
+axis[0,2].bar(x, y, width=.5)
+
+#make last spot empty
+axis[1,2].axis("off")
 
 # Adjust layout for better spacing
 plt.tight_layout()
--- reproductive_number.py
+++ reproductive_number.py
@@ -2,24 +2,24 @@
 #from scipy.integrate import odeint
 import numpy as np
 
-#mu =
-u_c = 0.03
-u_p = 0.03
-u_i = 0.033
+alpha_c = 0    #keep curcumin at a range of 0.1 to 0.2
 
-#delta = S
-S_a = 0.02 
-S_c = 0.015   
-S_p = 0.03  
-S_i = 0.01
+Beta = 0.0000012     # 0.00012   # 0.0001597
 
-alpha_c = 0.2   
-Beta = 0.20  
-r = 0.03
-lambda_p = 0.03
-lambda_i = 0.05
+# Deltas
+S_c = 0.9      # 0.9    # 0.015 
+S_p = 9e-6     # 9e-6   # 0.00399995 
+S_i = 3.162    #3.162   # 0.0527
 
+lambda_p = 18       #18       # 0.3
+lambda_i = 0.3      #0.3      # 0.005
+
+# mu 
+u_c = 1.8      # 1.8    # 0.03 per hr 
+
+r = 1.8             # 1.8     # 0.03
+
+#reproductive number
 r_0 = (u_c * (1 - alpha_c) + Beta * (lambda_p / S_p)) / ((S_c * (1 + alpha_c)) * (lambda_i / S_i)) 
 
-#print(r_0)
 
